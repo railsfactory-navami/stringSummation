c3d96c30bb8dadc3fda6daf8664b5035da1bfb96
railsfactory-navami <navami@railsfactory.org>
2018-03-02 17:25:59 +0530

    code added

--- lib/StringSummation.rb
+++ lib/StringSummation.rb
@@ -1,5 +1,12 @@
 require "StringSummation/version"
 
 module StringSummation
-  # Your code goes here...
+  def  self.add_string(value)
+   	if value.gsub(/[^0-9-]/, '').split('').include?('-')
+      raise  "Negative Number is not allowed"
+    else
+      result = value.to_s.each_char.map {|c| c.to_i }.reduce(:+) || 0
+    end
+    return result
+  end
 end
--- spec/StringSummation_spec.rb
+++ spec/StringSummation_spec.rb
@@ -3,7 +3,15 @@ RSpec.describe StringSummation do
     expect(StringSummation::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "should add string contrain special charecter" do
+    expect(StringSummation.add_string('1;2%/,,$$6,3')).to eql(12)
+  end
+
+  it "should return zero if string is empty" do
+    expect(StringSummation.add_string('')).to eql(0)
+  end
+
+  it "should throw error if string contains negative value" do
+    expect(StringSummation.add_string('-1;2%/,,$$6,3')).to raise_error("Negative Number is not allowed")
   end
 end
